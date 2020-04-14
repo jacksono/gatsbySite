cb4c402ec41848d3f729252d5190aaa37914cb52
Jackson Onyango <jackson.onyango@gmail.com>
2020-04-14 22:13:27 +0300

    step 0

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => (
+  <>
+    <Link to="/about/"> About me &rarr;</Link>
+    <h1> HOME </h1>
+    <p>Hell Frontend Masters!</p>
+  </>
+);
