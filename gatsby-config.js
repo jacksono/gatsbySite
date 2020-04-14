c3f92563709bc1c1a40cc154a9b6dada4545176a
Jackson Onyango <jackson.onyango@gmail.com>
2020-04-14 23:43:15 +0300

    step 2 - graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'JXN',
+    description: 'Me and my gatsby',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
