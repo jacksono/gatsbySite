b9da979a7a04e1f1f74561a278312a173edb8b91
Jackson Onyango <jackson.onyango@gmail.com>
2020-04-18 23:06:04 +0300

    mdx renderer

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,18 +3,31 @@ import { graphql } from 'gatsby';
 import { css } from '@emotion/core';
 import Layout from '../components/layout';
 import ReadLink from '../components/read-link';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 
-const PostTemplate = () => (
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        author
+      }
+      body
+    }
+  }
+`;
+
+const PostTemplate = ({ data: { mdx: post } }) => (
   <Layout>
-    <h1>Post title</h1>
+    <h1>{post.frontmatter.title}</h1>
     <p
       css={css`
         font-size: 0.75rem;
       `}
     >
-      Author by
+      Author by {post.frontmatter.author}
     </p>
-    <p> Post Body here </p>
+    <MDXRenderer>{post.body}</MDXRenderer>
     <ReadLink to="/"> &larr; back to all posts</ReadLink>
   </Layout>
 );
