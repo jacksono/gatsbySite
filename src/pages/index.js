1b7b6352bf5b9df058c429f5cf70f83da7f823a9
Jackson Onyango <jackson.onyango@gmail.com>
2020-04-23 23:55:06 +0300

    Add hero + image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,20 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import usePosts from '../hooks/usePosts';
 import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <Link to="/about/"> About me &rarr;</Link>
-      <h1> HOME </h1>
-      <p>Hell Frontend Masters!</p>
-
-      <h2> Read My Blog </h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2> Read My Blog </h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
